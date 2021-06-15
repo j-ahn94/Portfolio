5da5d660e12c77072673629cccf22ce319474a42
JasonAhn <jasonja.ahn@gmail.com>
2021-06-15 00:44:19 -0700

    Fix hamburger menu slide effect

--- main/src/main.js
+++ main/src/main.js
@@ -1,6 +1,13 @@
+
+const body = document.getElementById('body');
 const hamburger = document.getElementById('hamburger');
 const navUL = document.getElementById('menu-list')
+var closeMenu = document.getElementById('menuControlClose');
 
-hamburger.addEventListener('click', () => {
-    navUL.classList.toggle('show');
+closeMenu.addEventListener('click', function(e) {
+    navUL.classList.toggle('menu--active');
 });
+
+/*hamburger.addEventListener('click', () => {
+    navUL.classList.toggle('show');
+});*/
