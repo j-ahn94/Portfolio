f27dbec27df11a8b21dfaae5d3ed7a3440dfc89f
JasonAhn <jasonja.ahn@gmail.com>
2021-06-17 23:39:13 -0700

    Create about me page

--- main/src/main.js
+++ main/src/main.js
@@ -1,13 +1,5 @@
-
-const body = document.getElementById('body');
-const hamburger = document.getElementById('hamburger');
-const navUL = document.getElementById('menu-list')
-var closeMenu = document.getElementById('menuControlClose');
-
-closeMenu.addEventListener('click', function(e) {
-    navUL.classList.toggle('menu--active');
-});
-
-/*hamburger.addEventListener('click', () => {
-    navUL.classList.toggle('show');
-});*/
+class App {
+    constructor() {
+        this.canvas = document.createElemtn
+    }
+}
